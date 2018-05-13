6541781b627751a0c1c844a29ac2c28b59ecfce6
kubakolas <kubakolas12@gmail.com>
2018-05-13 22:51:04 +0200

    update

--- STM App/src/main.c
+++ STM App/src/main.c
@@ -1,73 +1,74 @@
 #include "tm_stm32f4_pcd8544.h"
 #include "ringtone.h"
 #include "Nokia_Sms.h"
-int d=0;
-int cyfra;
-char numer[13];
-int j = 0;
-int dzwoni = 0;
-int sms = 0;
-int dzwonek_sms=0;
 
-void USART1_IRQHandler(void) //odbior dzwoniacego numeru
+char sign;
+char number[13];
+unsigned int j = 0;
+unsigned int d = 0;
+uint8_t is_calling = 0;
+uint8_t sms_came = 0;
+uint8_t sms_ringtone=0;
+
+void USART1_IRQHandler(void)
 {
 	if(USART_GetITStatus(USART1, USART_IT_RXNE) != RESET)
 	{
-		cyfra = USART1->DR;
-		if(cyfra == 's') // jesli przyjdzie SMS a nie polaczenie
+		sign = USART1->DR;
+		if(sign == 's') // jesli przyjdzie SMS
 		{
-			sms=1; // flaga SMS
-			dzwonek_sms=1; // wlacza dzwonek ze przyszedl SMS
+			sms_came = 1;
+			sms_ringtone = 1;
 		}
-		if(sms == 0) //polaczenie, zczytywanie kolejnych cyfr numeru
+		if(!sms_came) //polaczenie, zczytywanie kolejnych cyfr numberu
 		{
-			numer[j] = cyfra;
+			number[j] = sign;
 			if(j == 11)
 			{
 				j = 0;
-				dzwoni = 1;
+				is_calling = 1;
 			}
-			else if(cyfra != 'e') j++;
-			if(cyfra == 'e')
+			else if(sign != 'e') j++;
+			if(sign == 'e')
 			{
-				dzwoni = 0;
+				is_calling = 0;
 				PCD8544_Clear();
 				PCD8544_GotoXY(0, 14);
 				PCD8544_Puts("PHONE INFORMER" , PCD8544_Pixel_Set, PCD8544_FontSize_5x7);
 				PCD8544_Refresh();
 			}
-			if(dzwoni == 1)
+			if(is_calling)
 			{
 
 				PCD8544_Clear();
 				PCD8544_GotoXY(0, 14);
-				PCD8544_Puts(numer, PCD8544_Pixel_Set, PCD8544_FontSize_5x7);
+				PCD8544_Puts(number, PCD8544_Pixel_Set, PCD8544_FontSize_5x7);
 				PCD8544_GotoXY(0, 28);
-				PCD8544_Puts("dzwoni..." , PCD8544_Pixel_Set, PCD8544_FontSize_5x7);
+				PCD8544_Puts("is_calling..." , PCD8544_Pixel_Set, PCD8544_FontSize_5x7);
 				PCD8544_Refresh();
 			}
 		}
 		else  // SMS
 		{
-					if(cyfra != 's') //pomija znacznik ze przyszedl SMS i zczytuje kolejne cyfry numeru nadawcy
-					{
-						numer[j] = cyfra;
-						if(j == 11)
-						{
-							j = 0;
-							sms = 0;
-						}
-						else j++;
-						if(sms == 0)
-						{
-							PCD8544_Clear();
-							PCD8544_GotoXY(0, 7);
-							PCD8544_Puts("Dostales SMS", PCD8544_Pixel_Set, PCD8544_FontSize_5x7);
-							PCD8544_GotoXY(0, 28);
-							PCD8544_Puts(numer, PCD8544_Pixel_Set, PCD8544_FontSize_5x7);
-							PCD8544_Refresh();
-						}
-					}
+			if(sign != 's') //pomija znacznik ze przyszedl SMS i zczytuje kolejne cyfry numberu nadawcy
+			{
+				number[j] = sign;
+				if(j == 11)
+				{
+					j = 0;
+					sms_came = 0;
+				}
+				else j++;
+				if(!sms_came)
+				{
+					PCD8544_Clear();
+					PCD8544_GotoXY(0, 7);
+					PCD8544_Puts("Dostales SMS", PCD8544_Pixel_Set, PCD8544_FontSize_5x7);
+					PCD8544_GotoXY(0, 28);
+					PCD8544_Puts(number, PCD8544_Pixel_Set, PCD8544_FontSize_5x7);
+					PCD8544_Refresh();
+				}
+			}
 		}
 	}
 }
@@ -75,23 +76,23 @@ void TIM3_IRQHandler(void) //dzwonek
 {
          	if(TIM_GetITStatus(TIM3, TIM_IT_Update) != RESET)
          	{
-         		if(dzwoni == 1)
+         		if(is_calling == 1)
          		{
          			DAC_Cmd(DAC_Channel_1, ENABLE);
-         			DAC_SetChannel1Data(DAC_Align_12b_R, rawData[d]*1.5); //dzwonek polaczenia
+         			DAC_SetChannel1Data(DAC_Align_12b_R, ringtone[d]*2); //dzwonek polaczenia
          			d++;
          			if(d > 105677) d = 0;
          		}
-         		else if(dzwonek_sms == 1)
+         		else if(sms_ringtone == 1)
          		{
          			DAC_Cmd(DAC_Channel_1, ENABLE);
          			DAC_SetChannel1Data(DAC_Align_12b_R, nokia_sms[d]*1.5); // dzwonek sms
          			d++;
          			if(d > 73661)
          				{
-         					dzwonek_sms=0;
-         					d=0;
-         				}
+         					sms_ringtone=0;
+         					d = 0;
+         				}//rs16 -> 3v
          		}
          		else
          		{
@@ -116,29 +117,15 @@ void EXTI0_IRQHandler(void)
 void GPIO_Configuration(void)
 {
   GPIO_InitTypeDef GPIO_InitStructure;
-
-
-  /* Configure USART1 Tx (PA.09) as alternate function push-pull */
   GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
   GPIO_Init(GPIOA, &GPIO_InitStructure);
-  // ustawienie funkcji alternatywnej dla pinów (USART)
+
   GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_USART1);
   GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_USART1);
-
-
-
-  GPIO_InitStructure.GPIO_Pin =
-  GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-  GPIO_Init(GPIOD, &GPIO_InitStructure);
-
 
   //inicjalizacja wyjœcia DAC
   GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
@@ -146,7 +133,6 @@ void GPIO_Configuration(void)
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
   GPIO_Init(GPIOA, &GPIO_InitStructure);
-
 
   EXTI_InitTypeDef EXTI_InitStructure;
   EXTI_InitStructure.EXTI_Line = EXTI_Line0;
@@ -159,7 +145,7 @@ void GPIO_Configuration(void)
 void USART_Configuration(void)
 {
   USART_InitTypeDef USART_InitStructure;
-  USART_InitStructure.USART_BaudRate = 9600;		// Baud Rate
+  USART_InitStructure.USART_BaudRate = 9600;
   USART_InitStructure.USART_WordLength = USART_WordLength_8b;
   USART_InitStructure.USART_StopBits = USART_StopBits_1;
   USART_InitStructure.USART_Parity = USART_Parity_No;
@@ -211,7 +197,6 @@ void TIM3_Configuration(void) // DAC
 	TIM_TimeBaseStructure.TIM_CounterMode =  TIM_CounterMode_Up;
 	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
 	TIM_Cmd(TIM3, ENABLE);
-
 }
 int main(void)
 {
@@ -230,15 +215,11 @@ int main(void)
 	DAC_Configuration();
 	TIM3_Configuration();
 
-	PCD8544_Init(0x25); // LCD
+	PCD8544_Init(0x25);
 	PCD8544_Clear();
 	PCD8544_GotoXY(0, 14);
 	PCD8544_Puts("PHONE INFORMER" , PCD8544_Pixel_Set, PCD8544_FontSize_5x7);
 	PCD8544_Refresh();
-	numer[12]='\0';
-
-    while(1)
-    {
-
-    }
+	number[12]='\0';
+    while(1){}
 }
